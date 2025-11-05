cf0e6de7435c6eb566f20420e20355c561c0e802
Zaiyan-1930 <zaiyanmashrafirahman@gmail.com>
2025-11-05 10:35:33 +0600

    kk

---
+++ Project/MortgageCalculator.java
@@ -0,0 +1,30 @@
+package Project;
+
+import.java.util.Scanner;
+
+public class MortgageCalculator {
+    
+    public static void main(String[]args){
+
+      Scanner scanner = new Scanner(System.in);
+       
+      System.out.println("Enter principal");
+      double principal= scanner.nextDouble();
+
+      System.out.println("Enter Annual Interest Rate");
+      float rate = scanner.nextFloat();
+      
+
+      
+
+
+
+
+
+
+
+
+
+    }
+
+}
